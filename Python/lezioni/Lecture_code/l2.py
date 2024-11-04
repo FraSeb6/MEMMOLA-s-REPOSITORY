1f6d4f45891dac7c728ca309d2b4aad37e492d0a
Francesco <francescosebastiano.memmola@studenti.unimi.it>
2024-11-04 20:21:42 +0100

    update

--- Python/lezioni/Lecture_code/l2.py
+++ Python/lezioni/Lecture_code/l2.py
@@ -1,61 +1,117 @@
+import sys
 
-#sttring manipulation
-s= 'stefano montaneli'
-w='radar'
-#rw= w[-1:0:-1]
-rw= w[::-1]
+# string manipulation
 
-#basic built-in functions
-print(s.capitalize())
-print(s.lower())
-print(s.upper())
+s = "STEfano montanelli"
 
-#string 
-print(s[0]) #only first element S
-print("result of print(s[1:4])",s[1:4])#
-print(s[-1])
-print(s[1:])
-print(s[:1])
-print(s[:-1])
-print("result of print(s[-1:-4])",s[-1:-4])#not sintatical incorrect but it dosen't give a result
-print("result of print(s[:5])",s[:5])
-print("result of print(s[-5:])",s[-5:])
-print("result of print(s[5:])",s[5:])
-print("result of print(s[:-5])",s[:-5])
-print("result of print(s[1:4])",s[1:4])
-print("result of print(s[1:4:2])",s[1:4:2])#skippa la cella 2
-print("result of print(s[::2])",s[::2]) #returns odd elements in the string [not in the index]
-print("result of print(s[-3:-6])",s[-3:-6])
-print("result of print(s[-3:-6:-2])",s[-3:-6:-2])
+# basic built-in functions
+print(s.capitalize())   # Stefano montanelli, function that capitalize the first letter of the string
+print(s.title())        # Stefano Montanelli, function that capitalize the first letter of each word
+print(s.lower())        # stefano montanelli, function that _lower_ the case of each letter in the string
+print(s.upper())        # STEFANO MONTANELLI, function that _upper_ the case of each letter in the string
+print(s.swapcase())     # stEFANO MONTANELLI, function that swap the case of each letter in the string (lower to upper and viceversa)
 
 
-print(s[0].upper()+s[1:])
-print("result of s[1:4]", s[1:4].lower())
+"""
+    you can work with a string as you work with lists. By definition in Python a string is seen as a list: 
+    every character in the string is seen as an element of a list.
+"""
+# string slicing (through list slicing), the same concept is applied to lists, tuples, dictionaries, etc.
+"""
+
+        [start:stop:step]
+
+"""
+s = "STEfano montanelli"
+
+print("result of S[0]: ", s[0])                 # S, first element of the string
+print("result of s[1:4]: ", s[1:4])             # TEf, from the second to the fourth element of the string
+print("result of s[-1]: ", s[-1])               # i, last element of the string
+print("result of s[-4:-1]: ", s[-4:-1])         # ell, from the fourth to the second element of the string
+print("result of s[-1:-4]: ", s[-1:-4])         # empty string, the step is positive and the start is greater than the stop, NOT ERROR
+print("result of s[1:]", s[1:])                 # TEfano montanelli, from the second element to the end of the string    
+print("result of s[:-1]", s[:-1])               # STEfano montanell, from the first element to the second last element of the string
+print("result of s[-5:]", s[-5:])               # nelli, from the fifth last element to the end of the string
+print("result of s[:-5]", s[:-5])               # STEfano monta, from the first element to the fifth last element of the string    
+print("result of s[5:]", s[5:])                 # no montanelli, from the sixth element to the end of the string
+print("result of s[:5]", s[:5])                 # STEfa, from the first element to the fifth element of the string
+print("result of s[1:4:2]: ", s[1:4:2])         # Tf, from the second to the fourth element of the string with step 2
+                                                # it prints from 1 up to 4, taking every 2 character, so the 1st and the 3rd
+"""     slicing by an interval like [1:4] means that we are not considering the 4th element of the string
+            but we are considering the 1st, 2nd and 3rd element of the string NOT INCLUDING THE 4
+"""
+
+print("result of s[1:4]: ", s[1:4])             # TEf, from the second to the fourth element of the string, EXCLUDED THE 4th
+print("result of s[::2]: ", s[::2])             # Sfno onal, from the first to the last element of the string with step 2 (even elements)
+print("result of s[1::2]: ", s[1::2])           # Tenoatnli, from the second to the last element of the string with step 2 (odd elements)
+print("result of s[-6:-2]: ", s[-6:-2])         # ntel, from the sixth last to the second last element of the string
+print("result of s[-6:-2:-2]: ", s[-6:-2:-2])   # empty string, the step is negative and the start is greater than the stop, NOT ERROR
+print("result of s[::-1]: ", s[::-1])           # illenatnom onafETS, from the last to the first element of the string with step -1 (reversed string)
 
 
-if w==rw:
-    print("they are palindrome")
-else:
-    print("they are not palindrome")
-    
-#since checking palindrome is a sort of function , I define a targeted specification
+
+# given a string in input, return true/false if it is a palindrome (case insensitive)
+# radar, otto, civic
+
+
 def check_pal(w):
-    w=w.lower()
-    rw=w[::-1]
-    if w==rw:
+    """check if a word in input is palindrome.
+
+    Keyword arguments:
+    string -- the word to evaluate
+
+    Return:
+    bool -- True when palindrome; False otherwise
+    """
+    w = w.lower() # convert the string to lower case
+    rw = w[::-1]
+
+    # as an alternative to the if statement:
+    # return w == rw
+    if w == rw:
         return True
-    else: 
-        return False    
-    
-def sheck_pal_iteration(w):
-    left=0
-    right= len(w)-1
+    else:
+        return False
+
+"""let's change this function and the algorith beyond it in order not to slice the string, but using a diffetent approach"""
+
+def check_pal_iteration(w):
+    """check if a word in input is palindrome through iteration.
+
+    Keyword arguments:
+    string -- the word to evaluate
+
+    Return:
+    bool -- True when palindrome; False otherwise
+    """
+    left = 0
+    right = len(w) - 1
     is_pal = True
+
     while left < right:
-        if w[left] !=w[right]:
-            is_pal= True
-            #stop
+        if w[left].lower() != w[right].lower():
+            is_pal = False
+            # break stops the while and goes to the next operation after the loop
             break
+
         left += 1
-        right -=1
-    return is_pal
+        right -= 1
+
+    return is_pal 
+
+
+# main code
+word = "abcd45dcba"
+
+if check_pal_iteration(word):
+    # have a look at the print function in Python:
+    # https://docs.python.org/3/tutorial/inputoutput.html
+    print(f"the '{word}' is palindrome")
+else:
+    print(f"the '{word}' is not palindrome")
+
+sys.exit()
+
+# more exercises on string manipulations:
+# https://www.geeksforgeeks.org/python-string-exercise/
+# use a serch engine for more
--- Python/lezioni/Lecture_code/l3.py
+++ Python/lezioni/Lecture_code/l3.py
@@ -1,117 +1,77 @@
 import sys
-import csv
 
+# use of lists
+items = [1, 2, 3, 4, 5, 6]          # list of integers, created with the square brackets []
+# alternative syntax
+items = list((1, 2, 3, 4, 5, 6))    # list of integers, created from a tuple with the list() function
+print(items)
 
-        
-#use of list
-items = list([1, 2, 3, 4, 5])
-print (items)
-items= ['summer', 'autumn', 'winter']
-len(items)
-print(items[0])
+# example with strings
+items = ["summer", "autumn", "winter"]
+print(len(items))                   # length of the list     
+print(items[0])                     # access the first element
 
-items.append('spring')
+items.append("spring")              # add an element at the end of the list   
 print(len(items))
+"""
+A list can contain any type of character: strings or integers
 
-#iterate over the list 
+    In the last code we printed the length (function `len()` returning the length of an object) of the list ‘items’ 
+    and the first item ([0])of the list, 
+    and then we added a new element to the list (`’spring’`)
+
+"""
+
+# iterate over the list
 for i in items:
     print(i)
-    
-#what is the output of this command 
-print (list(range(len(items)))) 
 
-#list sorting
-print (items)
+# what is the output of this command?
+print(list(range(len(items))))
+
+# list sorting
+print(items)
 items.sort()
 print(items)
 
-#discover how sorting is performed in Python
-#have a look at the quicksort, mergesort
-#excise 
+# discover how sorting is performed in Python
+# have a look at the quicksort, mergesort
+# exercise: try to implement quicksort as a custum function
 
-#list slicing
-#list of dwarfd
-dwarfs =['Doc', 'Bashful', 'Grumpy', 'Sneezy', 'Happy', 'Sleepy', 'Dopey']
+# list slicing
+# list of dwarfs
+dwarfs = ["Doc", "Bashful", " Grumpy", "Sneezy", "Happy", "Sleepy", "Dopey"]
+print(dwarfs)
 print(dwarfs[1:5])
 print(dwarfs[:5])
 print(dwarfs[5:])
-print(dwarfs[:5]+ dwarfs[5:])
+print(dwarfs[:5] + dwarfs[5:])
 print(dwarfs[:])
+print(dwarfs[1:5])
 print(dwarfs[1:5:2])
 print(dwarfs[-1])
 print(dwarfs[-5:-1])
-print(dwarfs[-1:-5:-1])# we start from the last element of the list ae we red from -1 to -5
+print(dwarfs[-1:-5:-1])
 print(dwarfs[::-1])
-#same syntax of the string
-#futher exrcise: given a list, reverse the left eith the right art (calculate the middle index, and then use slicing to reverse)
 
+# exercise: given a list, reverse the left with the right part
+# (calculate the middle index, and then use slicing to reverse)
 
-#list of list
-A=[[1,2,3],[4,5,6],[7,8,9]]
-print (A)
-print(A[1])#remember python stars to count from zero
-print (A[1][2])#the second square bracket selects the element present in the selected bracket by indicating the first bracket
+# list of lists
+A = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+
+print(A)
+print(A[1])
+print(A[1][2])
 print(A[0][-1])
-
-#
-#this two scripts do the same task
-last_colums=[]
+# get the items of the last column
+last_column = []
 for item in A:
-    #get the last item of the row
-    last_colums.append(item[-1])  #more redundant of the following  
-#in this case if A contains anything and the for doesn't excute, but still this cose would print out the list last column, as an empty one
+    # get the last item of the row
+    last_column.append(item[-1])
 
-column = [row[2] for row in A]
-# another way of writing a loop, scrolls over the items of A, and for each element it takes the last element of th erow (of each list within the list in our case) 
-print (last_colums)
-
-#letsgonsky do it in a real example
-#read a file and process the rows as list items
-
-#open the file and read the content 
-with open ('dataset/supermarket.csv', newline=",") as f:
-    reader = csv.reader(f, delimiter=',')
-    records =list(reader)
-print(records)
-#if the file is in the same folder as the one you are currently in then it will run currently
-#if not not it will return an error, then to run it correctly you'll have to refer to the right folder in which the data is located by starting
-#for instnce open ('dataset/supermarket.csv', newlines="")if the data you want to use in located in that folder
-print(records [0]) 
-#the first row might reult being the header of our table/matrix
-#how can we replace with the content of rows from the second row, wanting to pull 
-
-#how can i replace records with the content of rows from the second row, wating to pull of the first row since it's the header
-record = records[1:]
-
-def unique_values(my_list):
-    #we use lopps
-    values=[]
-    for item in my_list:
-        if item not in values:
-            values.append(item)
-    return values        
-
-def unique_values_by_set(my_list):
-    my_set = set(my_list)
-    return list (my_set)
-
-
-
-#create a list with the unique names of branches 
-branches= [row[1] for row in records]
-#print[branches]
-unique_branches = unique_values(branches)
-print (unique_branches)
-# build a list of sales by branch
-# build a list  of lits where each row is about a branch
-# each row contains a list of sales of the given branch
-# sales = [ [ 123, 45345, 2342], [2342,2352, 23525, 24234], [2342,3453, 34535] ]
-sales =[]
-#try to find a solution, discussion in the next class
-
-
-
-
-
+# same with list comprehension
+last_column = [row[-1] for row in A]
+print(last_column)
 
 sys.exit()
